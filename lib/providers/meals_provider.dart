66763bfd6053a1d694e8d93d70679e11faecc930
Hasan Baydoun <hasan-net2010@hotmail.com>
2025-02-24 10:21:33 +0300

    just a quick note

--- lib/providers/meals_provider.dart
+++ lib/providers/meals_provider.dart
@@ -2,4 +2,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:meals/data/dummy_data.dart';
 
+//this provider has never been used in the app
+//its for educational purposes only
 final mealsProvider = Provider((ref) => dummyMeals);
