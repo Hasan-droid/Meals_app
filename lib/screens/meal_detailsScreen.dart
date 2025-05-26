ba76704b79f9d1add0222b45624768da274256e5
Hasan Baydoun <hasan-net2010@hotmail.com>
2025-05-26 15:37:41 +0300

    204. Configuring Implicit Animations

--- lib/screens/meal_detailsScreen.dart
+++ lib/screens/meal_detailsScreen.dart
@@ -17,7 +17,19 @@ class MealDetailsScreen extends ConsumerWidget {
         title: Text(meal.title),
         actions: [
           IconButton(
-            icon: Icon(isFavorite ? Icons.star : Icons.star_border),
+            icon: AnimatedSwitcher(
+              duration: const Duration(milliseconds: 300),
+              transitionBuilder: (child, animation) {
+                return RotationTransition(
+                  turns: Tween(begin: 0.90, end: 1.0).animate(animation),
+                  child: child,
+                );
+              },
+              child: Icon(
+                isFavorite ? Icons.star : Icons.star_border,
+                key: ValueKey(isFavorite),
+              ),
+            ),
             onPressed: () {
               final result = ref
                   .read(favoriteMealsProvider.notifier)
